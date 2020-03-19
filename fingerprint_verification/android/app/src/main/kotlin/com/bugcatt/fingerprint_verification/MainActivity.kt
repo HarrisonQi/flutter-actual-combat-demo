c7156ee1703a373d1647c41b05a97d6089000d03
HarrisonQI <harrisonqi@foxmail.com>
2020-03-19 18:46:52 +0800

    :sparkles: 指纹验证demo项目

---
+++ fingerprint_verification/android/app/src/main/kotlin/com/bugcatt/fingerprint_verification/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bugcatt.fingerprint_verification
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterFragmentActivity;
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ fingerprint_verification/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:fingerprint_verification/fingerprint_util.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () => FingerPrintUtil.checkTouchId(),
+          child: Text("测试指纹支付"),
+        ),
+      ),
+    );
+  }
+}
